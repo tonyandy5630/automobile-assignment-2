f04f9ddbe6113a71b0482d80234ea9de1ba0c9e0
Chivvse151512 <97072169+Chivvse151512@users.noreply.github.com>
2022-11-06 21:49:04 +0700

    fix path

--- eStore/Startup.cs
+++ eStore/Startup.cs
@@ -50,7 +50,7 @@ namespace eStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
             });
         }
     }

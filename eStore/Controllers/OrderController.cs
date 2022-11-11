47ec74f5f14062cd251bee14f23f8a86cc97ea66
Chivvse151512 <97072169+Chivvse151512@users.noreply.github.com>
2022-11-11 14:17:44 +0700

    update order

--- eStore/Controllers/OrderController.cs
+++ eStore/Controllers/OrderController.cs
@@ -99,22 +99,40 @@ namespace eStore.Controllers
         }
 
         // GET: HomeController1/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orders = orderRepository.GetOrders();
+            OrderObject order = orders.FirstOrDefault(o => o.OrderId == id.Value);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
         }
 
         // POST: HomeController1/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(OrderObject order, IFormCollection collection)
         {
             try
             {
+                if (ModelState.IsValid)
+                {
+                    orderRepository.UpdateOrder(order);
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
+                ViewBag.Message = ex.Message;
                 return View();
             }
         }
@@ -135,8 +153,6 @@ namespace eStore.Controllers
             return View(order);
         }
 
-        // POST: HomeController1/Delete/5
-        //[Route("Order/Delete")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
